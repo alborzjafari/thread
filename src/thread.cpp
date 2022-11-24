0cbb01fc5f9f3fe28bcc697b99235c9a5c5571cb
Alborz Jafari <alborz.jafari@turboard.com>
2022-11-24 11:29:03 +0300

    Increase cmake version and fix coding style.

--- src/thread.cpp
+++ src/thread.cpp
@@ -2,44 +2,32 @@
 
 using namespace std;
 
-Thread::~Thread()
-{
-}
+Thread::~Thread() {}
 
-Thread::Thread() : is_running(true)
-{
-}
+Thread::Thread() : is_running(true) {}
 
-Thread::Thread(Thread&& src) noexcept : thread_object(move(src.thread_object))
-{
-}
+Thread::Thread(Thread&& src) noexcept
+  : thread_object(move(src.thread_object)) {}
 
-Thread& Thread::operator=(Thread&& rhs) noexcept
-{
+Thread& Thread::operator=(Thread&& rhs) noexcept {
   if (&rhs != this)
     thread_object = move(rhs.thread_object);
 
   return *this;
 }
 
-void Thread::start()
-{
+void Thread::start() {
   thread_object = make_unique<std::thread>(&Thread::entry_point, this);
 }
 
-void Thread::join()
-{
+void Thread::join() {
   thread_object->join();
   is_running.store(false);
 }
 
-bool Thread::running() const noexcept
-{
-  return is_running.load();
-}
+bool Thread::running() const noexcept { return is_running.load(); }
 
-void Thread::entry_point()
-{
+void Thread::entry_point() {
   run();
   is_running.store(false);
 }
